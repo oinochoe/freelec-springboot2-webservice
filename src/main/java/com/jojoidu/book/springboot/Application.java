bef664819fc68476bc9b3e9af38acb734b54c1ed
oinochoe <oinochoe@naver.com>
2021-01-24 21:21:40 +0900

    JpaRepository BaseTimeEntity 등을 통해 생성시간, 수정시간 추상화(abstract) => 다른 포인트에서 신경쓸 필요 없음.

--- src/main/java/com/jojoidu/book/springboot/Application.java
+++ src/main/java/com/jojoidu/book/springboot/Application.java
@@ -2,9 +2,10 @@ package com.jojoidu.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-// SpringBootApplication이 있는 위치부터 설정을 읽어가기 때문에 이클래스는 항상 프로젝트 최상단에 위치..
-@SpringBootApplication // Bean 읽기와 생성을 모두 자동으로 설정됩니다.
+@EnableJpaAuditing // JPA Auditing 전체 활성화
+@SpringBootApplication // Bean 읽기와 생성을 모두 자동으로 설정됩니다.// SpringBootApplication이 있는 위치부터 설정을 읽어가기 때문에 이클래스는 항상 프로젝트 최상단에 위치..
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -13,6 +14,6 @@ public class Application {
 
 
 // 내장 WAS란 ?
-// 별도로 외부에 Was를 두지 않고(tomcat 등), 애플리케이션을 실행할 때 내부에서 Was를 실행하는 것..
+// 별도로 외부에 Was를 두지 않고(tomcat 등), 애플리케   이션을 실행할 때 내부에서 Was를 실행하는 것..
 
 // 참고로 서비느는 비즈니스 로직을 처리하는게 아니라... 트랜잭션, 도메인간 순서의 보장만 해줍니다.
--- src/main/java/com/jojoidu/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/jojoidu/book/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.jojoidu.book.springboot.domain.posts;
 
+import com.jojoidu.book.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,7 +13,7 @@ import javax.persistence.*;
 @Getter // 롬복 -  클래스 내 모든 필드의 Getter메소드를 자동 생성
 @NoArgsConstructor // 롬복 - 기본 생성자 자동추가... public Posts {} 와 같은 효과
 @Entity // JPA의 어노테이션 - 테이블과 링크될 클래스임을 나타냄.... 기본값으로 클래스의 카멜케이스 이름을 언더스코어 네이밍으로 테이블 이름을 매칭
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id // 해당 테이블의 pk필드를 나타냅니다.
     @GeneratedValue(strategy = GenerationType.IDENTITY) // PK생성 규칙을 나타냅니다.. (auto_increment 위해 IDNNTITY 추가)
