decdfebf187ce42edd865c5578ea23b1f4d22afc
oinochoe <oinochoe@naver.com>
2021-01-31 23:24:08 +0900

    User 패키지 작성중

---
+++ src/main/java/com/jojoidu/book/springboot/domain/user/User.java
@@ -0,0 +1,24 @@
+package com.jojoidu.book.springboot.domain.user;
+
+import com.jojoidu.book.springboot.domain.BaseTimeEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Column;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+}
