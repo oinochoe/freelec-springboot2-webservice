c2728c08cad1cc6b8e31aee63cba1a5f40fb1597
oinochoe <oinochoe@naver.com>
2021-02-07 11:32:53 +0900

    소셜 로그인을 위한 의존성 추가 및 Role enum 관리. 그리고 UserRepository로 findByEmail 메서드 작성

--- src/main/java/com/jojoidu/book/springboot/domain/user/User.java
+++ src/main/java/com/jojoidu/book/springboot/domain/user/User.java
@@ -1,19 +1,17 @@
 package com.jojoidu.book.springboot.domain.user;
 
 import com.jojoidu.book.springboot.domain.BaseTimeEntity;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Column;
+import javax.persistence.*;
 
 @Getter
 @NoArgsConstructor
 @Entity
 public class User extends BaseTimeEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -24,5 +22,31 @@ public class User extends BaseTimeEntity {
     @Column(nullable = false)
     private String email;
 
+    @Column
+    private String picture;
 
+    // JPA로 데이터베이스를 지정할 때 Enum값을 어떤형태로 지정할지 결정 / 기본은 int
+    // 숫자로 저장되면 데이터베이스로 확인할 때 그 값이 무슨 코드를 의미하는지 알 수가 없음.
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Builder
+    public User(String name, String email, String picture, Role role) {
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+        this.role = role;
+    }
+
+    public User update(String name, String picture) {
+        this.name = name;
+        this.picture = picture;
+
+        return this;
+    }
+
+    public String getRoleKey() {
+        return this.role.getKey();
+    }
 }
