f60dbd83296c8cefe702da5cb3e56fc3d1ce7e6d
oinochoe <oinochoe@naver.com>
2021-02-27 19:04:42 +0900

    스프링 시큐리티 설정파일 오타 수정 및 메서드 확인

--- src/main/java/com/jojoidu/book/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/jojoidu/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -25,6 +25,9 @@ public class OAuthAttributes {
     }
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
     }
     // of() = OAuth2User에서 반환하는 사용자 정보는 Map이기 때문에 값 하나하나를 반환해야해서 of 사용
@@ -35,6 +38,17 @@ public class OAuthAttributes {
                 .email((String) attributes.get("email"))
                 .picture((String) attributes.get("picture"))
                 .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    public static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String,Object> response = (Map<String, Object>) attributes.get("response");
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
